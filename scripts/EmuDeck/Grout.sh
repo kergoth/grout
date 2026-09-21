e26aa74cdbb98c1d1ee42d79906cf4909d449a3f
Christopher Larson <kergoth@gmail.com>
2026-09-21 15:52:26 -0700

    deck: Ensure that we chmod +x the binary

--- scripts/EmuDeck/Grout.sh
+++ scripts/EmuDeck/Grout.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 scriptdir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 exec > >(tee -a "${scriptdir}/Grout/grout.log") 2>&1
+chmod +x "${scriptdir}/Grout/grout"
 CFW=EMUDECK LD_LIBRARY_PATH="${scriptdir}/Grout/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" exec "${scriptdir}/Grout/grout" "$@"
